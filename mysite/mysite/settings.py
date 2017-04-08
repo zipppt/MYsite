81b69dc5e25dbb554815dbdac71349b43fab3e88
Andrew Trofimchuk <zipppt@gmail.com>
2017-04-08 04:27:47 +0300

    fix setting

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
